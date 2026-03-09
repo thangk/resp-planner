1c2d3dd6aaeb360fc16379d9c75b2763bcfaf16d
Kap Thang <dev@kapthang.dev>
2026-03-08 22:31:45 -0400

    refactor: use Vercel rewrites for PostHog proxy instead of reverse proxy

--- src/components/providers/posthog-provider.tsx
+++ src/components/providers/posthog-provider.tsx
@@ -7,10 +7,7 @@ import { Suspense, useEffect } from 'react';
 
 if (typeof window !== 'undefined' && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
   posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
-    api_host:
-      process.env.NEXT_PUBLIC_POSTHOG_REVERSE_HOST ||
-      process.env.NEXT_PUBLIC_POSTHOG_HOST ||
-      'https://us.i.posthog.com',
+    api_host: '/ingest',
     ui_host: 'https://us.posthog.com',
     person_profiles: 'identified_only',
     capture_pageview: false, // We capture manually below
